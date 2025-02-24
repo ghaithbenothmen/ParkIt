079739945bcae17cb82f8c0b5548d29dc5aaaf22
ghaithbenothmen <ghaithbenothmen8@gmail.com>
2025-02-24 03:20:08 +0100

    feat: integration new template

---
+++ frontend/src/feature-module/frontend/pages/pages.routes.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import AboutUs from './about-us/about-us';
+
+import ContactUs from './contact-us/contact-us';
+
+import Pricing from './pricing/pricing';
+
+import Booking1 from './booking/booking-1';
+import SessionExpired from './session-expired/session-expired';
+import Error404 from './Error page/error404';
+import Error500 from './Error page/error500';
+
+import BookingDetails from './booking/booking-details';
+import PaymentSetting from '../providers/settings/payment-setting';
+
+import BookingWizard from './booking/booking-wizard';
+import Invoice from '../customers/invoice/invoice';
+
+
+const PagesRoutes = () => {
+  const all_pages_routes = [
+    {
+      path: '/about-us',
+      name: 'about-us',
+      element: <AboutUs />,
+      route: Route,
+    },
+
+   
+   
+    {
+      path: '/contact-us',
+      name: 'contact-us',
+      element: <ContactUs />,
+      route: Route,
+    },
+  
+    
+    {
+      path: '/invoice',
+      name: 'invoice',
+      element: <Invoice />,
+      route: Route,
+    },
+    
+    {
+      path: '/pricing-plan',
+      name: 'pricing',
+      element: <Pricing />,
+      route: Route,
+    },
+ 
+    {
+      path: '/booking',
+      name: 'booking',
+      element: <BookingWizard />,
+      route: Route,
+    },
+    {
+      path: '/booking/booking-1',
+      name: 'booking-1 ',
+      element: <Booking1 />,
+      route: Route,
+    },
+    {
+      path: '/booking/booking-details',
+      name: 'booking-details',
+      element: <BookingDetails />,
+      route: Route,
+    },
+    {
+      path: '/session-expired',
+      name: 'SessionExpired',
+      element: <SessionExpired />,
+      route: Route,
+    },
+
+    {
+      path: '/payment-setting',
+      name: 'payment-setting',
+      element: <PaymentSetting />,
+      route: Route,
+    },
+
+
+    {
+      path: '*',
+      name: 'NotFound',
+      element: <Navigate to="/" />,
+      route: Route,
+    },
+  ];
+
+  return (
+    <>
+      <Routes>
+        <Route>
+          {all_pages_routes.map((route, idx) => (
+            <Route path={route.path} element={route.element} key={idx} />
+          ))}
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default PagesRoutes;
