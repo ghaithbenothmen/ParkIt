4c24861e89b9335e6008a02bb5716e1c11eccd34
TahaBellotef <taha.bellotef@esprit.tn>
2025-04-27 18:02:59 +0100

    fix:face recognition

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -11,7 +11,7 @@ router.put("/profile", authMiddleware, userController.updateProfile);
 router.post("/logout", userController.logout);
 router.post("/register_face",userController.register_face_data);
 router.get("/verify/:token", userController.verifyActivation); // Ensure this route exists
-router.post("/login_face",userController.loginWithFace)
+router.post("/login_face",userController.loginWithFace);
 router.post("/verify-2fa", userController.verify2FA); 
 router.post("/enable-2fa", userController.enable2FA); 
 router.post("/disable-2fa", userController.disable2FA);
