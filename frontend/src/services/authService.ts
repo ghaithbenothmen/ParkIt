6f3fa477c324c93beb3db395f22a5992b875f9f8
ghaithbenothmen <ghaithbenothmen8@gmail.com>
2025-05-20 00:02:57 +0000

    fix: uri fix.

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/auth`; 
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/auth`; 
 
 
 export const register = async (userData: {
